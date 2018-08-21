ff34e030efb054a202b784d4657be7acdbc479d4
omAurion <42566779+omAurion@users.noreply.github.com>
2018-08-21 17:31:26 +0530

    Output file name changed

--- YesBankPrivateDnPdf/make_demo.bat
+++ YesBankPrivateDnPdf/make_demo.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd .
 cd bin
-ContentEncoderUtil.exe -t ..\..\build\template.html -u qwe -p qweqwe -i ..\sample_data.json -o ..\..\build\index.html
+ContentEncoderUtil.exe -t ..\..\build\template.html -u qwe -p qweqwe -i ..\sample_data.json -o ..\..\build\YPrivate.html
 popd
